b4f0db7966c9d7b57fe336a571765885012f869e
Tiji <143082629+IamTiji@users.noreply.github.com>
2026-06-06 17:11:29 +0800

    Move render handler to different class in osr test

--- java/tests/osr/TestBrowserImpl.java
+++ java/tests/osr/TestBrowserImpl.java
@@ -21,14 +21,17 @@ import java.util.function.Consumer;
  * It does not paint anything to screen, it is to check if certain methods
  * are called as expected.
  */
-public class TestBrowserImpl extends CefBrowser_N implements CefRenderHandler {
+public class TestBrowserImpl extends CefBrowser_N {
+    TestRenderHandlerImpl renderHandler;
+
     protected TestBrowserImpl(CefClient client, String url) {
         super(client, url, CefRequestContext.getGlobalContext(), null, null, null);
+        renderHandler = new TestRenderHandlerImpl();
     }
 
     @Override
     public CefRenderHandler getRenderHandler() {
-        return this;
+        return renderHandler;
     }
 
     @Override
@@ -52,70 +55,5 @@ public class TestBrowserImpl extends CefBrowser_N implements CefRenderHandler {
     @Override
     public CompletableFuture<BufferedImage> createScreenshot(boolean nativeResolution) {
         return null;
-    }
-
-    @Override
-    public Rectangle getViewRect(CefBrowser browser) {
-        return new Rectangle(0, 0, 300, 300);
-    }
-
-    @Override
-    public boolean getScreenInfo(CefBrowser browser, CefScreenInfo screenInfo) {
-        return false;
-    }
-
-    @Override
-    public Point getScreenPoint(CefBrowser browser, Point viewPoint) {
-        return null;
-    }
-
-    @Override
-    public void onPopupShow(CefBrowser browser, boolean show) {
-
-    }
-
-    @Override
-    public void onPopupSize(CefBrowser browser, Rectangle size) {
-
-    }
-
-    @Override
-    public void onPaint(CefBrowser browser, boolean popup, Rectangle[] dirtyRects, ByteBuffer buffer, int width, int height) {
-
-    }
-
-    @Override
-    public void addOnPaintListener(Consumer<CefPaintEvent> listener) {
-
-    }
-
-    @Override
-    public void setOnPaintListener(Consumer<CefPaintEvent> listener) {
-
-    }
-
-    @Override
-    public void removeOnPaintListener(Consumer<CefPaintEvent> listener) {
-
-    }
-
-    @Override
-    public boolean onCursorChange(CefBrowser browser, int cursorType) {
-        return false;
-    }
-
-    @Override
-    public boolean startDragging(CefBrowser browser, CefDragData dragData, int mask, int x, int y) {
-        return false;
-    }
-
-    @Override
-    public void updateDragCursor(CefBrowser browser, int operation) {
-
-    }
-
-    @Override
-    public void onAcceleratedPaint(CefBrowser browser, boolean popup, Rectangle[] dirtyRects, CefAcceleratedPaintInfo info) {
-        System.out.println(info.shared_texture_handle);
     }
 }
---
+++ java/tests/osr/TestRenderHandlerImpl.java
@@ -0,0 +1,79 @@
+package tests.osr;
+
+import org.cef.browser.CefBrowser;
+import org.cef.browser.CefPaintEvent;
+import org.cef.callback.CefDragData;
+import org.cef.handler.CefRenderHandler;
+import org.cef.handler.CefScreenInfo;
+import org.cef.misc.CefAcceleratedPaintInfo;
+
+import java.awt.*;
+import java.nio.ByteBuffer;
+import java.util.function.Consumer;
+
+public class TestRenderHandlerImpl implements CefRenderHandler {
+    @Override
+    public Rectangle getViewRect(CefBrowser browser) {
+        return new Rectangle(0, 0, 300, 300);
+    }
+
+    @Override
+    public boolean getScreenInfo(CefBrowser browser, CefScreenInfo screenInfo) {
+        return false;
+    }
+
+    @Override
+    public Point getScreenPoint(CefBrowser browser, Point viewPoint) {
+        return null;
+    }
+
+    @Override
+    public void onPopupShow(CefBrowser browser, boolean show) {
+
+    }
+
+    @Override
+    public void onPopupSize(CefBrowser browser, Rectangle size) {
+
+    }
+
+    @Override
+    public void onPaint(CefBrowser browser, boolean popup, Rectangle[] dirtyRects, ByteBuffer buffer, int width, int height) {
+
+    }
+
+    @Override
+    public void addOnPaintListener(Consumer<CefPaintEvent> listener) {
+
+    }
+
+    @Override
+    public void setOnPaintListener(Consumer<CefPaintEvent> listener) {
+
+    }
+
+    @Override
+    public void removeOnPaintListener(Consumer<CefPaintEvent> listener) {
+
+    }
+
+    @Override
+    public boolean onCursorChange(CefBrowser browser, int cursorType) {
+        return false;
+    }
+
+    @Override
+    public boolean startDragging(CefBrowser browser, CefDragData dragData, int mask, int x, int y) {
+        return false;
+    }
+
+    @Override
+    public void updateDragCursor(CefBrowser browser, int operation) {
+
+    }
+
+    @Override
+    public void onAcceleratedPaint(CefBrowser browser, boolean popup, Rectangle[] dirtyRects, CefAcceleratedPaintInfo info) {
+        System.out.println(info.shared_texture_handle);
+    }
+}
